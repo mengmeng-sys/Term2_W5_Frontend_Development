403edcc96b405bcb5f1bc4a585a844df1d08f8b6
mengmeng-sys <na634997@gmail.com>
2026-02-20 10:06:03 +0700

    last change on 6 7 8

--- Lab5.6/src/App.jsx
+++ Lab5.6/src/App.jsx
@@ -5,10 +5,23 @@ import './App.css'
 
 function App() {
 
+const [post, setPost] = useState([])
+const [newContent,setNewContent] = useState("")
+const handleAdd = ()=>{
+  const item = {id:post.length,text:newContent}
+  setPost(lastitem=>[...lastitem,item])
+  setNewContent("")
 
+}
   return (
     <>
-
+      <input type="text" placeholder='Enter the content post' value={newContent} onChange={e=>setNewContent(e.target.value)}/>
+      <button onClick={handleAdd}>Add item</button>
+      <ul>
+        {
+          post.map(({id, text}, index)=><li key={index}>{id} : {text}</li>)
+        }
+      </ul>
     </>
   )
 }
--- Lab5.7/src/App.jsx
+++ Lab5.7/src/App.jsx
@@ -4,30 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [search,setSearch] = useState("")
+ const items = ["React","JavaScript","Tailwind"];
+ 
+ const foundItem = items.filter(item =>{
+    return item.toLowerCase().includes(search.toLowerCase())
+ })
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <input type="text" placeholder='Search your items' value={search} onChange={e=>setSearch(e.target.value)} />
+     <ul>
+      {search != "" && foundItem.map((val,index) =>(<li key={index}>{val}</li>))}
+     </ul>
     </>
   )
 }
