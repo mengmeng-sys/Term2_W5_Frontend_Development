70cd863ee486e55005c02c463ff60a4f99a6bbb0
mengmeng-sys <na634997@gmail.com>
2026-02-20 13:29:45 +0700

    lab5.9 and ex2

--- Lab5.9/src/App.jsx
+++ Lab5.9/src/App.jsx
@@ -4,32 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState([{ id: 1 }, { id: 2 }, { id: 3 }]);
+
+  const removeItem = (id) => {
+    setList(list.filter(item => item.id !== id));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {list.map(item => (
+        <div key={item.id}>
+          <span>Item {item.id}</span>
+          <button onClick={() => removeItem(item.id)}>Delete</button>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App
