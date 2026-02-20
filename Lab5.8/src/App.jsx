f5c397fe581a6522bb18b45f2c9dc91769b43798
mengmeng-sys <na634997@gmail.com>
2026-02-20 10:45:09 +0700

    Change lab5.8

--- Lab5.8/src/App.jsx
+++ Lab5.8/src/App.jsx
@@ -1,14 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [posts, setPosts] = useState([{ id : 1, text:"hello", isEdited:false}])
+  const [posts, setPosts] = useState([
+    { id: 1, text: "Hello", isEdited: false },
+    { id: 2, text: "World", isEdited: false },
+    { id: 3, text: "React", isEdited: false },
+  ])
+  const [id, setID] = useState(0)
+
+  const markAsEdited = (id) => {
+    const postTemp = posts.map(post => {
+      if (post.id === id) {
+        alert(`Post id:${post.id} isEdited changed from ${post.isEdited} to ${!post.isEdited}`)
+        return { ...post, isEdited: !post.isEdited }
+      }
+      return post
+    })
+    setPosts(postTemp)
+  }
 
   return (
     <>
-       
+      <input 
+        type="text" 
+        placeholder='Enter the post ID to edit' 
+        onChange={e => setID(e.target.value)} 
+      />
+      <button onClick={() => markAsEdited(Number(id))}>Mark</button>     
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>
+            ID: {post.id} | Text: {post.text} | isEdited: {post.isEdited.toString()}
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
